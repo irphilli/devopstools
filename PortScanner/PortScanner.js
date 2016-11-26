ef119d825196146c5c2fe15d0272e68879e57fb0
Phil Phillips <pphillips@experts-exchange.com>
2016-11-26 05:49:52 +0000

    portscanner should handle raw ip

--- PortScanner/PortScanner.js
+++ PortScanner/PortScanner.js
@@ -1,8 +1,7 @@
-console.log("Loading Function");
-
 var pn = require("port-numbers");
 var ps = require("portscanner");
 var dns = require("dns");
+var ip = require("ip");
 
 var portMax = 65535;
 var defaults = [
@@ -32,45 +31,54 @@ exports.handler = function(event, context, callback) {
    var host = event.host;
    var port = event.port;
 
-   // Host is required
+   if (ip.isV4Format(host) || ip.isV6Format(host)) {
+      if (ip.isPrivate(host)) {
+         callback("Invalid Host");
+         return;
+      }
+      runIpScan(host, port, callback);
+      return;
+   }
+
    dns.lookup(host, function(err, address) {
       if (err) {
-         // callback("Invalid Host");
+         callback("Invalid Host");
          return;
       }
       console.log(host + " resolved to " + address);
-      if (port == null) {
-         console.log("Default");
-         var results = [];
-         defaults.forEach(function(port) {
-            scanPort(host, port, function(result) {
-               results.push(result);
-               if (results.length == defaults.length) {
-                  console.log(results.sort(compare));
-                  //callback(null, result);
-               }
-            });
-         });
-      }
-      else {
-         if (isNaN(port) || port < 0 || port > portMax) {
-            //callback("Invalid port number.");
-            return;
-         }
-         port = Number(port);
-         scanPort(address, port, function(result) {
-            console.log(result);
-            //callback(null, result);
-         });
-      }
+      runIpScan(address, port, callback);
    });
+}
+
+function runIpScan(address, port, callback) {
+   if (port == null) {
+      console.log("Default");
+      var results = [];
+      defaults.forEach(function(port) {
+         scanPort(address, port, function(result) {
+            results.push(result);
+            if (results.length == defaults.length) {
+               callback(null, results.sort(compare));
+            }
+         });
+      });
+   }
+   else {
+      if (isNaN(port) || port < 0 || port > portMax) {
+         callback("Invalid port number.");
+         return;
+      }
+      port = Number(port);
+      scanPort(address, port, function(result) {
+         callback(null, result);
+      });
+   }
 }
 
 var scanPort = function(address, port, callback) {
    console.log("Scanning " + address + ":" + port);
 
    ps.checkPortStatus(port, address, function (err, status) {
-      console.log(port);
       var portInfo = pn.getService(port);
 
       var result = {};
@@ -90,4 +98,7 @@ var event = {
 //   port: "80",
    host: "www.experts-exchange.com"
 };
-exports.handler(event, null, null);
+exports.handler(event, null, function(err, result) {
+   console.log(err);
+   console.log(result);
+});
