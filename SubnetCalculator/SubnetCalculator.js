62f3f8bcd2a2273a003e54dc448b0bbfdbcedd18
Phil Phillips <pphillips@experts-exchange.com>
2016-11-26 05:34:43 +0000

    change subnet calculator to return json

--- SubnetCalculator/SubnetCalculator.js
+++ SubnetCalculator/SubnetCalculator.js
@@ -1,7 +1,4 @@
-console.log("Loading Function");
-
 var calc = require( 'ip-subnet-calculator' );
-var sprintf = require("sprintf-js").sprintf;
 
 var format = "%-25s";
 
@@ -12,7 +9,7 @@ exports.handler = function(event, context, callback) {
    var ipInfo = calc.calculateSubnetMask(ip, prefixSize);
    if (ipInfo == null) {
       // Invalid input
-      // callback("Bad IP or Netmask");
+      callback("Bad IP or Netmask");
       return;
    }
 
@@ -20,50 +17,26 @@ exports.handler = function(event, context, callback) {
    var hostMin = calc.toString(ipInfo.ipLow + 1);
    var hostMax = calc.toString(ipInfo.ipHigh - 1);
 
-   var output = "<pre>\n";
+   var result = {
+      ip: ip,
+      netmask: ipInfo.prefixMaskStr,
+      cidr: ipInfo.prefixSize,
+      wildcard: ipInfo.invertedMaskStr,
+      network: ipInfo.ipLowStr,
+      broadcast: ipInfo.ipHighStr,
+      firstHost: hostMin,
+      lastHost: hostMax,
+      numHosts: numberOfHosts
+   };
 
-   output += "<font color=\"#000000\">Address:    </font>\n";
-   output += "<font color=\"#0000ff\">" + sprintf(format, ip) + "</font>\n";
-   output += "<br/>\n";
-
-   output += "<font color=\"#000000\">Netmask:    </font>\n";
-   output += "<font color=\"#0000ff\">" + sprintf(format, ipInfo.prefixMaskStr + " (/" + ipInfo.prefixSize + ")") + "</font>\n";
-   output += "<br/>\n";
-
-   output += "<font color=\"#000000\">:Wildcard:  </font>\n";
-   output += "<font color=\"#0000ff\">" + sprintf(format, ipInfo.invertedMaskStr) + "</font>\n";
-   output += "<br/>\n";
-
-   output += "<br/><br/>-------------------------------------------<br/><br/>\n";
-
-   output += "<font color=\"#000000\">Network:    </font>\n";
-   output += "<font color=\"#0000ff\">" + sprintf(format, ipInfo.ipLowStr) + "</font>\n";
-   output += "<br/>\n";
-
-   output += "<font color=\"#000000\">Broadcast:  </font>\n";
-   output += "<font color=\"#0000ff\">" + sprintf(format, ipInfo.ipHighStr) + "</font>\n";
-   output += "<br/>\n";
-   
-   output += "<font color=\"#000000\">First Host: </font>\n";
-   output += "<font color=\"#0000ff\">" + sprintf(format, hostMin) + "</font>\n";
-   output += "<br/>\n";
-
-   output += "<font color=\"#000000\">Last Host:  </font>\n";
-   output += "<font color=\"#0000ff\">" + sprintf(format, hostMax) + "</font>\n";
-   output += "<br/>\n";
-
-   output += "<font color=\"#000000\"># of Hosts: </font>\n";
-   output += "<font color=\"#0000ff\">" + sprintf(format, numberOfHosts) + "</font>\n";
-   output += "<br/>\n";
-
-   output += "</pre>";
-
-   console.log(output);
-//   callback(null, output);
+   callback(null, result);
 }
 
 var event = {
    ip: "192.168.0.2",
    prefixSize: "24"
 };
-exports.handler(event, null, null);
+exports.handler(event, null, function(err, result) {
+   console.log(err);
+   console.log(result);
+});
