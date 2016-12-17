c3cbb147a84b940baf09500e000b455445cc93c7
Phil Phillips <pphillips@experts-exchange.com>
2016-12-17 19:43:09 +0000

    update downdetector output

--- DownDetector/DownDetector.js
+++ DownDetector/DownDetector.js
@@ -2,11 +2,35 @@ var http = require("http");
 var https = require("https");
 var url = require('url');
 
+var statusCodes = {
+   400: "Bad Request",
+   401: "Unauthorized",
+   403: "Forbidden",
+   404: "Not Found",
+   409: "Conflict",
+   500: "Internal Server Error",
+   501: "Not implemented",
+   502: "Bad Gateway",
+   503: "Service Unavailable",
+   504: "Gateway Timeout"
+};
+
 exports.handler = function(event, context, callback) {
    if (!event.host.startsWith("http"))
       event.host = "http://" + event.host;
 
    var urlInfo = url.parse(event.host);
+   console.log(urlInfo);
+   if (urlInfo.host == null || urlInfo.host == '')
+   {
+      callback("Invalid host");
+      return;
+   }
+   if (urlInfo.path != "/")
+   {
+      callback("Input must be hostname or IP only");
+      return;
+   }
 
    var options = {
       host: urlInfo.host,
@@ -16,29 +40,44 @@ exports.handler = function(event, context, callback) {
 
    var req = connector.request(options, function(res) {
       var statusCode = res.statusCode;
+      var result;
       if (statusCode >= 400) {
-         var result = {
+         var reason = (statusCodes[statusCode] != null) ? 
+            "Bad status code (" + statusCode + ": " + statusCodes[statusCode] + ")" :
+            "Bad status code (" + statusCode + ")";
+
+         result = {
             host: urlInfo.protocol + "://" + urlInfo.host,
             up: false,
-            reason: "bad status code (" + statusCode + ")"
+            reason: reason,
          };
-         callback(null, result);
-         return;
       }
-
-      var result = {
-         host: urlInfo.protocol + "://" + urlInfo.host,
-         up: true,
-         statusCode: statusCode
-      };
+      else {
+         result = {
+            host: urlInfo.protocol + "://" + urlInfo.host,
+            up: true,
+            statusCode: statusCode
+         };
+      }
       callback(null, result);
    });
    req.on("error", function (err) {
-      var result = {
-         host: urlInfo.protocol + "://" + urlInfo.host,
-         up: false,
-         reason: err.syscall + " failed with code " + err.code
-      };
+      var result;
+      if (err.syscall == "getaddrinfo") {
+         result = {
+            host: urlInfo.protocol + "://" + urlInfo.host,
+            up: false,
+            reason: "Could not resolve domain",
+            hint: "Double check that domain is typed correctly"
+         };
+      }
+      else {
+         result = {
+            host: urlInfo.protocol + "://" + urlInfo.host,
+            up: false,
+            reason: err.syscall + " failed with code " + err.code
+         };
+      }
       callback(null, result);
    });
    req.end();
@@ -46,8 +85,10 @@ exports.handler = function(event, context, callback) {
 
 /*
 var event = {
+//   host: "<alert>script('test');</alert>"
    host: "a"
-//   host: "https://www.experts-exchange.com/test.html"
+//   host: "a.com/test.html"
+//   host: "https://www.experts-exchange.com"
 //   host: "http://experts-exchange-437318971.us-east-1.elb.amazonaws.com"
 };
 exports.handler(event, null, function(err, result) {
