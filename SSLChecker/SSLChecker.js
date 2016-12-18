53e62fcbc7cda08555a49a4c35e8d7caefd87a80
Phil Phillips <pphillips@experts-exchange.com>
2016-12-18 05:57:18 +0000

    add ciphers to output

--- SSLChecker/SSLChecker.js
+++ SSLChecker/SSLChecker.js
@@ -1,5 +1,3 @@
-console.log("Loading Function");
-
 var https = require("https");
 var sslinfo = require("sslinfo");
 var exec = require("child_process").exec;
@@ -25,61 +23,117 @@ exports.handler = function(event, context, callback) {
       port: port,
       method: 'GET'
    };
-
-   var completedAttrs = 0;
-   var result = {};
-
    var req = https.request(options, function(res) {
-      var certificateInfo = res.connection.getPeerCertificate(true);
-      result.https = true;
-
-      completedAttrs += attrs.https;
-      if (completedAttrs == allAttrs)
-         callback(null, result);
+      var certificateInfo = res.connection.getPeerCertificate();
+      checkSSL(host, port, false, false, true, certificateInfo, callback);
    });
    req.on("error", function (err) {
-      callback("Could not connect to " + host + ":" + port);
+      var expired = (err.code == "CERT_HAS_EXPIRED");
+      var notYetValid = (err.code == "CERT_NOT_YET_VALID");
+      var chainValid = expired || notYetValid;
+      checkSSL(host, port, expired, notYetValid, chainValid, null, callback);
    });
    req.end();
+};
+
+function checkSSL(host, port, expired, notYetValid, chainValid, certificateInfo, callback) {
+   process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+   var result = {
+      expired: expired,
+      notYetValid: notYetValid,
+      chainValid: chainValid,
+      tls1: false,
+      tls1_1: false,
+      tls1_2: false
+   }; 
+
+   var error = false;
+   var completedAttrs = 0;
+
+   var options = {
+      host: host,
+      port: port,
+      method: 'GET'
+   };
+
+   if (certificateInfo == null) {
+      var req = https.request(options, function(res) {
+         var certificateInfo = res.connection.getPeerCertificate();
+         console.log(certificateInfo);
+
+         completedAttrs += attrs.https;
+         if (completedAttrs == allAttrs)
+            callback(null, result);
+      });
+      req.on("error", function (err) {
+         if (!error) {
+            error = true;
+            callback("Could not connect to " + host + ":" + port);
+         }
+      });
+      req.end();
+   }
+   else {
+      completedAttrs += attrs.https;
+      console.log(certificateInfo);
+   }
 
    sslinfo.getServerResults(options).
       done(function(res) {
-         result.cert = true;
+         var ciphers = new Set();
+         Object.keys(res.ciphers).forEach(function(item) {
+            if (res.ciphers[item].enabled.length > 0) {
+               if (item == "TLSv1_method")
+                  result.tls1 = true;
+               else if (item == "TLSv1_1_method")
+                  result.tls1_1 = true;
+               else if (item == "TLSv1_2_method")
+                  result.tls1_2 = true;
+            }
+            res.ciphers[item].enabled.forEach(function(cipher) {
+               ciphers.add(cipher);
+            });
+         });
+
+         result.ciphers = Array.from(ciphers);
 
          completedAttrs += attrs.cert;
          if (completedAttrs == allAttrs)
             callback(null, result);
       },
       function(err) {
-         callback("Could not connect to  " + host + ":" + port);
+         if (!error) {
+            error = true;
+            callback("Could not connect to " + host + ":" + port);
+         }
       }
    );
 
-   exec("echo | openssl s_client -connect " + host + ":" + port + "-ssl2", function(err) {
-      if (err) {
-         console.log("no ssl 2");
-      }
+   exec("echo | openssl s_client -connect " + host + ":" + port + " -ssl2", function(err) {
+      result.ssl2 = (err == null);
       completedAttrs += attrs.v2;
       if (completedAttrs == allAttrs)
          callback(null, result);
    });
 
-   exec("echo | openssl s_client -connect " + host + ":" + port + "-ssl3", function(err) {
-      if (err) {
-         console.log("no ssl 3");
-      }
+   exec("echo | openssl s_client -connect " + host + ":" + port + " -ssl3", function(err) {
+      result.ssl3 = (err == null);
       completedAttrs += attrs.v3;
       if (completedAttrs == allAttrs)
          callback(null, result);
    });
 };
 
-/*
 var event = {
-   host: "www.experts-exchange.com"
+//   host: "www.experts-exchange.com"
+//   host: "expired.badssl.com"
+//   host: "self-signed.badssl.com"
+//   host: "incomplete-chain.badssl.com"
+   host: "untrusted-root.badssl.com"
+//   host: "revoked.badssl.com"
 };
 exports.handler(event, null, function(err, result) {
    console.log(err);
    console.log(result);
 });
-*/
