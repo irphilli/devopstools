023b585bd3739568b7314c079592a2dbca98909b
Phil Phillips <pphillips@experts-exchange.com>
2017-02-01 18:46:35 +0000

    do portscan before down detection

--- DownDetector/DownDetector.js
+++ DownDetector/DownDetector.js
@@ -1,6 +1,7 @@
 var http = require("http");
 var https = require("https");
 var url = require('url');
+var ps = require("portscanner");
 
 var ua = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
 
@@ -36,53 +37,73 @@ exports.handler = function(event, context, callback) {
    var options = {
       host: urlInfo.host,
       port: (urlInfo.protocol == "http:") ? 80 : 443,
-      headers: {'User-Agent': ua}
+      headers: {'User-Agent': ua},
+      timeout: 1000
    };
    var connector = (urlInfo.protocol == "http:") ? http : https;
 
-   var req = connector.request(options, function(res) {
-      var statusCode = res.statusCode;
-      var result;
-      if (statusCode >= 400) {
-         var reason = (statusCodes[statusCode] != null) ? 
-            "Bad status code (" + statusCode + ": " + statusCodes[statusCode] + ")" :
-            "Bad status code (" + statusCode + ")";
+   ps.checkPortStatus(options.port, options.host, function (err, status) {
+      if (err || status != "open") {
+         result = {
+            host: urlInfo.protocol + "//" + urlInfo.host,
+            up: false,
+            reason: urlInfo.host + " isn't listening on port " + options.port
+         };
+         callback(null, result);
+         return;
+      }
 
-         result = {
-            host: urlInfo.protocol + "//" + urlInfo.host,
-            up: false,
-            reason: reason,
-         };
-      }
-      else {
-         result = {
-            host: urlInfo.protocol + "//" + urlInfo.host,
-            up: true,
-            statusCode: statusCode
-         };
-      }
-      callback(null, result);
+      var req = connector.request(options, function(res) {
+         var statusCode = res.statusCode;
+         var result;
+         if (statusCode >= 400) {
+            var reason = (statusCodes[statusCode] != null) ? 
+               "Bad status code (" + statusCode + ": " + statusCodes[statusCode] + ")" :
+               "Bad status code (" + statusCode + ")";
+
+            result = {
+               host: urlInfo.protocol + "//" + urlInfo.host,
+               up: false,
+               reason: reason,
+            };
+         }
+         else {
+            result = {
+               host: urlInfo.protocol + "//" + urlInfo.host,
+               up: true,
+               statusCode: statusCode
+            };
+         }
+         callback(null, result);
+      });
+      req.on("error", function (err) {
+         var result;
+         if (err.syscall == "getaddrinfo") {
+            result = {
+               host: urlInfo.protocol + "//" + urlInfo.host,
+               up: false,
+               reason: "Could not resolve domain",
+               hint: "Double check that domain is typed correctly"
+            };
+         }
+         else if (err.reason) {
+            result = {
+               host: urlInfo.protocol + "//" + urlInfo.host,
+               up: false,
+               reason: err.reason
+            };
+         }
+         else {
+            result = {
+               host: urlInfo.protocol + "//" + urlInfo.host,
+               up: false,
+               reason: err.syscall + " failed with code " + err.code
+            };
+         }
+         callback(null, result);
+      });
+      req.end();
    });
-   req.on("error", function (err) {
-      var result;
-      if (err.syscall == "getaddrinfo") {
-         result = {
-            host: urlInfo.protocol + "//" + urlInfo.host,
-            up: false,
-            reason: "Could not resolve domain",
-            hint: "Double check that domain is typed correctly"
-         };
-      }
-      else {
-         result = {
-            host: urlInfo.protocol + "//" + urlInfo.host,
-            up: false,
-            reason: err.syscall + " failed with code " + err.code
-         };
-      }
-      callback(null, result);
-   });
-   req.end();
 };
 
 /*
@@ -92,7 +113,9 @@ var event = {
 //   host: "a.com/test.html"
 //   host: "https://www.experts-exchange.com"
 //   host: "http://experts-exchange-437318971.us-east-1.elb.amazonaws.com"
-   host: "stackoverflow.com"
+//   host: "stackoverflow.com"
+//   host: "carol.ns.cloudflare.com"
+   host: "https://8.8.8.8"
 };
 exports.handler(event, null, function(err, result) {
    console.log(err);
